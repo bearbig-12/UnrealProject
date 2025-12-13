04a6a2dcf4918569632ab434dfaf1eb0c0cc4893
Hyeontae An <83248120+bearbig-12@users.noreply.github.com>
2025-12-13 20:11:22 +0900

    SLGame Update
    초기 세팅 업데이트

---
+++ SLGame/Source/SLGame/Player/SLGPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SLGPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLGAME_API ASLGPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
---
+++ SLGame/Source/SLGame/SLGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SLGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SLGame, "SLGame" );
